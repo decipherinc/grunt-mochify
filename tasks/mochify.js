1c829956afcada7bdbe3f229eb21b62331728f12
Christopher Hiller <chiller@decipherinc.com>
2015-08-03 11:53:29 -0700

    fix reporter output newlines; unparse transform args

--- tasks/mochify.js
+++ tasks/mochify.js
@@ -1,24 +1,27 @@
 'use strict';
 
 var spawn = require('child-process-promise').spawn;
-var map = require('lodash.map');
+var unparseArgs = require('unparse-args');
 
 function mochifyTask(grunt) {
 
   grunt.registerMultiTask('mochify', 'Run Mochify', function (target) {
     var done = this.async();
+    var options = this.options();
+    var args;
+    var mochifyPath;
 
-    var options = this.options({
-      reporter: 'spec',
-      colors: !!process.stdout.isTTY,
-      debug: target === 'debug'
-    });
+    if (process.stdout.isTTY) {
+      options.colors = true;
+    }
 
-    var args = map(options, function (value, name) {
-      return '--' + name + '=' + value;
-    }).concat(this.filesSrc);
+    if (target === 'debug') {
+      options.debug = true;
+    }
 
-    var mochifyPath = require.resolve('mochify/bin/cmd');
+    args = unparseArgs(options).concat(this.filesSrc);
+
+    mochifyPath = require.resolve('mochify/bin/cmd');
 
     grunt.verbose.writeln('Spawning Mochify with command %s\n\tand ' +
       'arguments: %s', mochifyPath, grunt.log.wordlist(args));
@@ -26,10 +29,10 @@ function mochifyTask(grunt) {
     spawn(mochifyPath, args)
       .progress(function (childProcess) {
         childProcess.stdout.on('data', function (data) {
-          console.log(String(data));
+          process.stdout.write(String(data));
         });
         childProcess.stderr.on('data', function (data) {
-          console.error(String(data));
+          process.stderr.write(String(data));
         });
       })
       .then(done)
