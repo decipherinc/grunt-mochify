af34e4c84ee199e6c22c22700f5d2bcda046afcb
Christopher Hiller <chiller@decipherinc.com>
2015-10-13 13:18:00 -0700

    try to avoid problems on windows; update dev deps

--- tasks/mochify.js
+++ tasks/mochify.js
@@ -1,15 +1,13 @@
 'use strict';
 
-var spawn = require('child-process-promise').spawn;
 var unparseArgs = require('unparse-args');
+var ketch = require('ketch');
 
 function mochifyTask(grunt) {
 
-  grunt.registerMultiTask('mochify', 'Run Mochify', function (target) {
+  grunt.registerMultiTask('mochify', 'Run Mochify', function(target) {
     var done = this.async();
     var options = this.options();
-    var args;
-    var mochifyPath;
 
     if (process.stdout.isTTY) {
       options.colors = true;
@@ -19,28 +17,29 @@ function mochifyTask(grunt) {
       options.debug = true;
     }
 
-    args = unparseArgs(options).concat(this.filesSrc);
-
-    mochifyPath = require.resolve('mochify/bin/cmd');
-
-    grunt.verbose.writeln('Spawning Mochify with command %s\n\tand ' +
-      'arguments: %s', mochifyPath, grunt.log.wordlist(args));
-
-    spawn(mochifyPath, args)
-      .progress(function (childProcess) {
-        childProcess.stdout.on('data', function (data) {
+    ketch(process.execPath)
+      .append(require.resolve('mochify/bin/cmd'))
+      .append(unparseArgs(options))
+      .append(this.filesSrc)
+      .tap(function(k) {
+        var args = k.serialize();
+        grunt.verbose.writeln('Spawning Mochify with command %s\n\tand ' +
+          'arguments: %s', args[0], grunt.log.wordlist(args[1]));
+      })
+      .spawn()
+      .progress(function(childProcess) {
+        childProcess.stdout.on('data', function(data) {
           process.stdout.write(String(data));
         });
-        childProcess.stderr.on('data', function (data) {
+        childProcess.stderr.on('data', function(data) {
           process.stderr.write(String(data));
         });
       })
       .then(done)
-      .fail(function (err) {
+      .fail(function(err) {
         grunt.fail.warn(err);
       });
   });
-
 }
 
 module.exports = mochifyTask;
